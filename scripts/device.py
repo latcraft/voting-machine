f9520f44511788bed0cadd53c1295d41650247de
Andrey Adamovich <andrey@aestasit.com>
2016-11-09 20:15:18 +0200

    device updates

--- scripts/device.py
+++ scripts/device.py
@@ -78,9 +78,15 @@ PIGLOW_LED_OFF      = 0
 PIGLOW_LED_ON       = 255
 
 # SparkFun constants
-SF_RED_BUTTON       = 18
-SF_YELLOW_BUTTON    = 23
-SF_GREEN_BUTTON     = 24
+SF_RED_LED          = 17
+SF_YELLOW_LED       = 27
+SF_GREEN_LED        = 22
+SF_LEDS             = [SF_RED_LED, SF_YELLOW_LED, SF_GREEN_LED]
+SF_LED_OFF          = 0
+SF_LED_ON           = 1
+SF_RED_BUTTON       = 16
+SF_YELLOW_BUTTON    = 20
+SF_GREEN_BUTTON     = 21
 SF_BUTTONS          = [SF_RED_BUTTON, SF_YELLOW_BUTTON, SF_GREEN_BUTTON]
 SF_BUTTON_NAMES     = ['RED',         'YELLOW',         'GREEN'        ]
 
@@ -107,10 +113,15 @@ def initBoards():
     elif (config['board'] == 'grove'):    
       Grove.pinMode(int(config['grove_led']), "OUTPUT")
       Grove.pinMode(int(config['grove_buzzer']), "OUTPUT")    
-      # TODO: Grove.rtc_getTime()
-    elif (config['board'] == 'sparkfun'): 
+      print Grove.rtc_getTime()
+    elif (config['board'] == 'sparkfun'):
+      print Grove.rtc_getTime()
+      Grove.pinMode(int(config['grove_buzzer']), "OUTPUT")
       GPIO.setwarnings(False)
       GPIO.setmode(GPIO.BCM)   
+      for led in SF_LEDS:
+        GPIO.setup(led, GPIO.OUT)
+        GPIO.output(led, SF_LED_OFF)
    
 def nfcReaders(vendorId, productId):
   for device in usb.core.find(find_all=True):
@@ -129,34 +140,44 @@ def blinkPiDieLed(color = 0, timeout = 1):
   sleep(timeout)
   GPIO.output(PIDIE_LEDS[color], PIDIE_LED_OFF)   
 
+def blinkSparkFunLed(color = 0, timeout = 1):
+  GPIO.output(SF_LEDS[color], SF_LED_OFF)
+  GPIO.output(SF_LEDS[color], SF_LED_ON)
+  sleep(timeout)
+  GPIO.output(SF_LEDS[color], SF_LED_OFF)
+
+def beepGroveBuzzer(color = 0, timeout = 1):
+  buzzer = int(config['grove_buzzer'])
+  Grove.digitalWrite(buzzer, 1)
+  sleep(timeout)
+  Grove.digitalWrite(buzzer, 0)
+
+def blinkSparkFunLedAndBeepGroveBuzzer(color = 0, timeout = 1):
+  buzzer = int(config['grove_buzzer'])
+  GPIO.output(SF_LEDS[color], SF_LED_OFF)
+  GPIO.output(SF_LEDS[color], SF_LED_ON)
+  Grove.digitalWrite(buzzer, 1)
+  sleep(timeout)
+  Grove.digitalWrite(buzzer, 0)
+  GPIO.output(SF_LEDS[color], SF_LED_OFF)
+
+
 def blinkGroveLedAndBeepGroveBuzzer(color = 0, timeout = 1):
   light = int(config['grove_led'])
   buzzer = int(config['grove_buzzer'])    
   Grove.digitalWrite(buzzer, 1)
-  Grove.analogWrite(light, 255)  
+  Grove.analogWrite(light, 255)
   sleep(timeout)
   Grove.digitalWrite(buzzer, 0)
   Grove.analogWrite(light, 0)
 
-def buttonReaderThread(button, actionFunction, timeout):
-  GPIO.setup(PIDIE_BUTTONS[button], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-  prev_input = GPIO.input(PIDIE_BUTTONS[button])
+def buttonReaderThread(button, buttonPins, buttonNames, actionFunction, timeout):
+  GPIO.setup(buttonPins[button], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+  prev_input = GPIO.input(buttonPins[button])
   while active:
-    input = GPIO.input(PIDIE_BUTTONS[button])
+    input = GPIO.input(buttonPins[button])
     if ((not prev_input) and input):
-      logging.info(str(PIDIE_BUTTON_NAMES[button]) + ' pressed')
-      if actionFunction is not None:
-        actionFunction(button, timeout)
-    prev_input = input
-    sleep(0.05)    
-
-def buttonReaderThread2(button, actionFunction, timeout):
-  GPIO.setup(SF_BUTTONS[button], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-  prev_input = GPIO.input(SF_BUTTONS[button])
-  while active:
-    input = GPIO.input(SF_BUTTONS[button])
-    if ((not prev_input) and input):
-      logging.info(str(SF_BUTTON_NAMES[button]) + ' pressed')
+      logging.info(str(buttonNames[button]) + ' pressed')
       if actionFunction is not None:
         actionFunction(button, timeout)
     prev_input = input
@@ -180,7 +201,7 @@ logging.info('Starting IOT service')
 
 initBoards()
 
-timeout = 0.3
+timeout = 0.1
 actionFunction = None
 if (config['board'] == 'pidie'):
   actionFunction = blinkPiDieLed
@@ -188,16 +209,18 @@ elif (config['board'] == 'piglow'):
   actionFunction = blinkPiGlowLed
 elif (config['board'] == 'grove'):
   actionFunction = blinkGroveLedAndBeepGroveBuzzer
+elif (config['board'] == 'sparkfun'):
+  actionFunction = blinkSparkFunLedAndBeepGroveBuzzer
 
 if (config['board'] == 'pidie'):
   for idx, button in enumerate(PIDIE_BUTTONS):
-    thread = Thread(target = buttonReaderThread, args = (idx, actionFunction, timeout))
+    thread = Thread(target = buttonReaderThread, args = (idx, PIDIE_BUTTONS, PIDIE_BUTTON_NAMES, actionFunction, timeout))
     thread.daemon = True
     thread.start()
 
 if (config['board'] == 'sparkfun'):
   for idx, button in enumerate(SF_BUTTONS):
-    thread = Thread(target = buttonReaderThread2, args = (idx, None, timeout))
+    thread = Thread(target = buttonReaderThread, args = (idx, SF_BUTTONS, SF_BUTTON_NAMES, actionFunction, timeout))
     thread.daemon = True
     thread.start()
 
